11ab4152c09c7c940800dfe08544be1457348f9f
jake9356 <janina.keidel@study.hs-duesseldorf.de>
2022-11-29 18:15:06 +0100

    generator

--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 from transformers import pipeline
 
+# analyses if a text has a good or bad mood
 classifier = pipeline("sentiment-analysis")
 print(classifier("It is a bad and good day!"))
 
@@ -8,6 +9,11 @@ print(classifier("It is a bad and good day!"))
 # /home/mirevi/janina/gpt_j_test_app/venv/lib/python3.10/site-packages/transformers/modeling_tf_utils.py 
 # comment out: #from keras.saving.hdf5_format import save_attributes_to_hdf5_group
 
+# classification in a text with selected labels
 classifier = pipeline("zero-shot-classification")
 print(classifier("We are going to the zoo today.",
         candidate_labels=["animals", "politics", "kids"]))
+
+# generates a text to a given text
+generator = pipeline("text-generation")
+print(generator("It was a beautiful day and"))
